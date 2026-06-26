78242c6facfba844551e1bf2bdb850a51ab9fb74
amritasrc <amritakumari13022006@gmail.com>
2026-06-26 12:37:35 +0530

    feat: add GitHub profile card UI

--- src/components/GithubSearch.jsx
+++ src/components/GithubSearch.jsx
@@ -13,7 +13,7 @@ const GithubSearch = () => {
         try {
             const response = await axios.get(`https://api.github.com/users/${username}`);
             setProfile(response.data);
-           // console.log(response.data);
+            console.log(response.data);
             setError(null);
         }
 
@@ -26,10 +26,12 @@ const GithubSearch = () => {
 
     return (
         <div className='w-full flex justify-center items-center flex-col'>
-            <h1 className='crafty-girls-regular p-4 pt-10 sm:pt-14 md:pt-14 lg:pt-10 text-4xl text-zinc-200 font-bold underline underline-offset-8'>
-                <span className='text-yellow-400 font-extrabold text-5xl'>GitProfile</span> - A GitHub Profile Finder</h1>
+
+            <h1 className='commissioner p-4 pt-10 sm:pt-14 md:pt-14 lg:pt-10 text-4xl text-zinc-200 font-semibold underline underline-offset-8'>
+                <span className='text-yellow-400 font-extrabold text-5xl crafty-girls-regular'>GitProfile</span> &mdash; A GitHub Profile Finder</h1>
 
             <form onSubmit={(e) => submitHandler(e)} className='mt-10 flex flex-col items-center sm:flex-row gap-10'>
+
                 <input type="text"
                     placeholder='Enter GitHub username...'
                     value={username}
@@ -41,9 +43,59 @@ const GithubSearch = () => {
                     className='commissioner bg-purple-600 hover:bg-purple-700 text-white px-5 py-2 rounded cursor-pointer'>
                     Search
                 </button>
+
             </form>
 
-            {error && <p className='text-zinc-100 text-2xl mt-10'>{error}</p>}
+            {error && <p className='commissioner text-zinc-100 text-2xl mt-10'>{error}</p>}
+            {profile &&
+
+                // profile container
+                <div className='profile-container mt-10 text-zinc-50 commissioner p-8 w-180 h-auto my-0 mx-auto rounded-xl shadow-xl shadow-zinc-900'>
+
+                    {/* profile content */}
+                    <div className='flex gap-7 justify-start'>
+
+                        {/* profile image */}
+                        <div className='h-full'>
+                            <img src={profile.avatar_url}
+                                alt="profile-avatar"
+                                className="w-32 h-32 rounded-full border-[3px] border-zinc-500 shadow-xl shadow-zinc-900" />
+                        </div>
+
+                        {/* profile details */}
+                        <div className='flex flex-col'>
+
+                            {/* profile description */}
+                            <div className='flex justify-between items-center gap-25'>
+
+                                <h2 className='text-3xl font-semibold'>{profile.name}</h2>
+                                <p>
+                                    Joined:{" "}
+                                    {new Date(profile.created_at).toLocaleDateString("en-US", {
+                                        year: "numeric",
+                                        month: "long",
+                                        day: "numeric",
+                                    })}
+                                </p>
+                            </div>
+
+                            <a href={profile.html_url}
+                                target='_blank'
+                                className='text-zinc-300 mt-2.5'>@{profile.login}</a>
+
+                            <p className='mt-10 mr-5 mb-20 ml-1.25 text-lg leading-6 text-zinc-200 font-normal'>{profile.bio}</p>
+
+                            {/* profile stats */}
+                            <div className='profile-stats flex justify-evenly items-center h-25 w-125 rounded-xl text-black text-center shadow-xl shadow-zinc-900 text-[13px] leading-10'>
+                                <p>Repositories <br /> <span className='stats'>{profile.public_repos}</span></p>
+                                <p>Followers <br /> <span className='stats'>{profile.followers}</span></p>
+                                <p>Following <br /> <span className='stats'>{profile.following}</span></p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            }
 
         </div>
     )
