97baf6ef2b5ebe1cb520f05f6066d7743ea47c4e
amritasrc <amritakumari13022006@gmail.com>
2026-06-26 08:26:08 +0530

    stye: heading

--- src/components/GithubSearch.jsx
+++ src/components/GithubSearch.jsx
@@ -11,7 +11,8 @@ const GithubSearch = () => {
 
     return (
         <div className='w-full flex justify-center items-center flex-col'>
-            <h1 className='crafty-girls-regular p-4 pt-10 sm:pt-14 md:pt-14 lg:pt-10 text-4xl text-white font-bold underline underline-offset-8'>GitProfile - A GitHub Profile Finder</h1>
+            <h1 className='crafty-girls-regular p-4 pt-10 sm:pt-14 md:pt-14 lg:pt-10 text-4xl text-zinc-200 font-bold underline underline-offset-8'>
+                <span className='text-yellow-400 font-extrabold text-5xl'>GitProfile</span> - A GitHub Profile Finder</h1>
 
             <form onSubmit={(e) => submitHandler(e)} className='mt-10 flex flex-col items-center sm:flex-row gap-10'>
                 <input type="text"
@@ -22,7 +23,7 @@ const GithubSearch = () => {
                 />
 
                 <button
-                    className='commissioner bg-purple-600 hover:bg-purple-700 text-white px-5 py-2 rounded'>
+                    className='commissioner bg-purple-600 hover:bg-purple-700 text-white px-5 py-2 rounded cursor-pointer'>
                     Search
                 </button>
             </form>
