74c5543eda86165a41848c75b26cdbbb76fd3742
amritasrc <amritakumari13022006@gmail.com>
2026-06-26 13:43:26 +0530

    feat: display profile details dynamically

--- src/components/GithubSearch.jsx
+++ src/components/GithubSearch.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
 import axios from 'axios';
+import { IoLocationSharp } from "react-icons/io5";
+import { IoBusinessSharp } from "react-icons/io5";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
 
 const GithubSearch = () => {
 
@@ -56,7 +60,7 @@ const GithubSearch = () => {
                     <div className='flex gap-7 justify-start'>
 
                         {/* profile image */}
-                        <div className='h-full'>
+                        <div className='h-full shrink-0'>
                             <img src={profile.avatar_url}
                                 alt="profile-avatar"
                                 className="w-32 h-32 rounded-full border-[3px] border-zinc-500 shadow-xl shadow-zinc-900" />
@@ -81,15 +85,52 @@ const GithubSearch = () => {
 
                             <a href={profile.html_url}
                                 target='_blank'
-                                className='text-zinc-300 mt-2.5'>@{profile.login}</a>
+                                className='text-zinc-300 mt-1'>@{profile.login}</a>
 
-                            <p className='mt-10 mr-5 mb-20 ml-1.25 text-lg leading-6 text-zinc-200 font-normal'>{profile.bio}</p>
+                            <p className='mt-5 mb-5 ml-1.25 text-lg leading-6 text-zinc-200 font-normal w-full'>{profile.bio}</p>
 
                             {/* profile stats */}
-                            <div className='profile-stats flex justify-evenly items-center h-25 w-125 rounded-xl text-black text-center shadow-xl shadow-zinc-900 text-[13px] leading-10'>
+                            <div className='profile-stats flex justify-evenly items-center h-25 w-125 mt-5 rounded-xl text-black text-center shadow-xl shadow-zinc-900 text-[13px] leading-10'>
                                 <p>Repositories <br /> <span className='stats'>{profile.public_repos}</span></p>
                                 <p>Followers <br /> <span className='stats'>{profile.followers}</span></p>
                                 <p>Following <br /> <span className='stats'>{profile.following}</span></p>
+                            </div>
+
+                            {/* profile info */}
+                            <div className='flex justify-start mt-10 gap-20'>
+                                <p className='flex gap-1 items-center'>
+                                    <IoLocationSharp />
+                                    {profile.location}
+                                </p>
+                                <p className="flex gap-1 items-center">
+                                    <IoBusinessSharp />
+                                    {profile.company || "Not specified"}
+                                </p>
+                            </div>
+
+                            {/* profile links */}
+                            <div className='flex flex-col gap-5 mt-10'>
+                                {profile.twitter_username ? (
+                                    <a
+                                        href={`https://x.com/${profile.twitter_username}`}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="bg-amber-400 hover:bg-amber-500 text-black py-2 px-3 flex gap-1 items-center w-fit rounded-full"
+                                    >
+                                        <FaXTwitter />
+                                        @{profile.twitter_username}
+                                    </a>
+                                ) : (
+                                    <p className="bg-zinc-700 text-zinc-300 py-2 px-3 flex gap-1 items-center w-fit rounded-full">
+                                        <FaXTwitter />
+                                        X Profile Not Available
+                                    </p>
+                                )}
+                                <a
+                                    href={profile.html_url}
+                                    target='_blank'
+                                    className='bg-amber-400 hover:bg-amber-500 text-black py-2 px-3 flex gap-1 items-center w-fit rounded-full'
+                                ><FaGithub />View Profile</a>
                             </div>
                         </div>
 
